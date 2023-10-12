82a20715de2ba360a31724865bb81c0afdb950b5
Айгуль <@DESKTOP-F7174NF>
2023-10-12 23:45:41 +0300

    Добавьте файлы проекта.

---
+++ Tymakov6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Tymakov6
+{
+    internal class Program
+    {
+        static int CountVowel(char[] text)
+        {
+            List<char> vowel = new List<char>() {'а', 'е', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'};
+            int count = 0;
+            foreach (char c in text) 
+            { 
+                if (vowel.Contains(char.ToLower(c)))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static int CountConsonant(char[] text)
+        {
+            List<char> consonant = new List<char>() {'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л','м', 'н', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'};
+            int count = 0;
+            foreach (char c in text)
+            {
+                if (consonant.Contains(char.ToLower(c)))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Задача 1. Программа считает количество гласных и согласных в файле.");
+            Console.WriteLine("Введите название файла:");
+            string file = Console.ReadLine();
+            if (string.IsNullOrEmpty(file))
+            {
+                Console.WriteLine("Файл не найден");
+            }
+
+            string file1 = File.ReadAllText(file);
+            char[] text = file1.ToCharArray();
+            int vowelcount = CountVowel(text);
+            int consonantcount = CountConsonant(text);
+            
+            Console.WriteLine("Количество гласных букв {0}", vowelcount);
+            Console.WriteLine("Количество согласных букв {0}", consonantcount);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ lesson5/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson5
+{
+    class Matrix
+    {
+        public int[,] A;
+        public int[,] B;
+    }
+    
+        Matrix matrix(int[,] a, int[,] b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public void PrintMatrix()
+        {
+            Console.WriteLine("Матрица А:");
+            for (int i = 0; i < A.GetLength(0), i++)
+            {
+                for (int j = 0; j < A.GetLength(1), j++)
+                {
+                    Console.Write(A[i, j] + "t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Матрица B:");
+            for (int i = 0; i < B.GetLength(0);  i++)
+            {
+                for (int j = 0; j < B.GetLengt(1);  j++)
+                {
+                    Console.Write(B[i, j] + "t");
+                }
+                Console.WriteLine();
+            }
+        }
+    
+        public bool CheckMatrix()
+        {
+            if (A.GetLength(1) != B.GetLength(0))
+            {
+                Console.WriteLine("Матрицы невозможно перемножить");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public interface[,] Multiply()
+        {
+            int[,] result = int[A.GetLength(0), B.GetLength(1)];
+            for (int i = 0; i < A.GetLength(0), i++)
+            {
+                for (int j = 0; j < B.GetLengt(1); j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < A.GetLength(1);  k++)
+                    {
+                        sum += A[i, k] * B[k, j];
+                    }
+                    result[i, j] = sum;
+                }
+
+            }
+
+        return result;
+        }
+    
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+           
+            
+            }
+        }
+}
